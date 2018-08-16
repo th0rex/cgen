47fdcdb7ca4897fce0eb78ab9e6e30bd82c643ec
th0rex <c0untlizzi@gmail.com>
2018-08-16 22:41:51 +0200

    update local.py to apply to imported functions as well

--- local.py
+++ local.py
@@ -1,12 +1,16 @@
+def un_dll_name(x):
+    if "!" in x and "@IAT" in x:
+        return x[x.find("!") + 1 : x.find("@IAT")]
+    elif "!" in x:
+        return x[x.find("!") + 1:]
+    return x
+
 def load_stuff(platform, type_imports=False):
-    stuff = bv.platform.parse_types_from_source_file("/vm/Programming/Python/cgen/{}.c".format(platform))
-    def un_dll_name(x):
-        if "!" in x:
-            return x[x.find("!")+1:x.find("@IAT")]
-        return x
+    stuff = bv.platform.parse_types_from_source_file( "/vm/Programming/Python/cgen/{}.c".format(platform))
     for name, t in stuff.types.items():
         bv.define_type(Type.generate_auto_type_id("source", name), name, t)
-    for fn in bv.functions:
+    fns = bv.functions + [bv.get_function_at(s.address) for s in bv.get_symbols_of_type(SymbolType.ImportedFunctionSymbol)]
+    for fn in fns:
         f_name = un_dll_name(fn.name)
         if f_name in stuff.functions:
             fn.set_user_type(stuff.functions[f_name])
@@ -17,3 +21,4 @@ def load_stuff(platform, type_imports=False):
             if s_name in stuff.functions:
                 p = Type.pointer(bv.arch, stuff.functions[s_name], const=True)
                 bv.define_user_data_var(symbol.address, p)
+
